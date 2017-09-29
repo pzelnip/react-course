d04fb061a7ec2b23b7fcd45c38ec23cb4f2a7d69
Adam Parkin <adam.parkin@bambora.com>
2017-09-29 12:37:23 -0700

    End of Lec 13

--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,12 +1,12 @@
 console.log("App.js is running");
 
-var app = {
+const app = {
     title: "Indecision App!",
     subtitle: "This is some info formatted!",
     options: ['One', 'Two']
 };
 
-var template = (
+const template = (
     <div>
         <h1>{app.title}</h1>
         {app.subtitle && <p>{app.subtitle}</p>}
@@ -20,21 +20,21 @@ function getLocation(user) {
     }
 }
 
-var user = {
+const user = {
     name: "Adam Parkin",
     age: "17",
     location: "Victoria"
 };
 
-var template2 = (
+const template2 = (
     <div>
         <h1>{user.name ? user.name : 'Anonymous'}</h1>
         {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
         {getLocation(user)}
     </div>
 );
-var appRoot = document.getElementById("app");
-var appRoot2 = document.getElementById("app2");
+const appRoot = document.getElementById("app");
+const appRoot2 = document.getElementById("app2");
 
 ReactDOM.render(template, appRoot);
 ReactDOM.render(template2, appRoot2);
